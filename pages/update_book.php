5ee1ac4e81e2c5ddf664f364549be9c9cecfc562
Pedro Ferari <151549155+Fedro2@users.noreply.github.com>
2024-06-28 00:02:40 +0200

    Update update_book.php

--- pages/update_book.php
+++ pages/update_book.php
@@ -73,7 +73,7 @@ $kategorien = $kategorienStmt->fetchAll();
             <a href="catalog.php">CATALOG</a>
             <a href="about.php">ABOUT</a>
         </div>
-        <a class="logo" href="../home.php">BOOP</a>
+        <a class="logo" href="../index.php">BOOP</a>
     
     <div class="admin-login">
         <?php if (isset($_SESSION["username"])): ?>
