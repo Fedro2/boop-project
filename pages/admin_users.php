966b9e3563678068b39bf1b164d8310dbeb2a2f6
Pedro Ferari <151549155+Fedro2@users.noreply.github.com>
2024-06-28 00:00:37 +0200

    Update admin_users.php

--- pages/admin_users.php
+++ pages/admin_users.php
@@ -101,21 +101,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <body>
 <main>
 <nav>
-    <div class="margin">
-        <a href="catalog.php">CATALOG</a>
-        <a href="about.php">ABOUT</a>
-    </div>
-    <a class="logo" href="../home.php">BOOP</a>
-
+        <div class="margin">
+            <a href="catalog.php">CATALOG</a>
+            <a href="about.php">ABOUT</a>
+        </div>
+        <a class="logo" href="../index.php">BOOP</a>
+    
     <div class="admin-login">
         <?php if (isset($_SESSION["username"])): ?>
             <?php if ($_SESSION["admin"] == 1): ?>
-                <a href="admin.php">
-                    <div class="admin">
-                        <img src="../pics/admin.png" alt="admin">
-                    </div>
-                </a>
-            <?php endif; ?>
+            <a href="admin.php">
+                <div class="admin">
+                    <img src="../pics/admin.png" alt="admin">
+                </div>
+            </a>
+        <?php endif; ?>
             <div class="login">
                 <a class="user" href="#"><?= $_SESSION["name"] . " " ?> <?= $_SESSION["surname"]?></a>
                 <div class="dropdown">
@@ -129,7 +129,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             </div>
         <?php endif; ?>
     </div>
-</nav>
+    </nav>
 
 <main>  
     <div class="admin-content">
